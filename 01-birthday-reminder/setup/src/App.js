f774f48cc1ee038baf917f90d3f1cfac173a9bdf
Papa <papa@Papas-MacBook-Pro.local>
2023-10-30 15:53:29 +0530

    fixes

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -4,13 +4,13 @@ import List from './List';
 function App() {
   const [people,setPeople] = useState(data)
   return (
-  	<div>
+  	<main>
   		<section className='container'>
   		<h3>{people.length} have birthdays today</h3>
   		<List people={people} />
   		<button onClick={() => {setPeople([])}}>Clear All</button>
   		</section>
-  	</div>
+  	</main>
   )
 }
 

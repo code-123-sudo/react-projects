886c2dd8ac4acce9dd13f8993f06fd20b6c1fff0
Papa <papa@Papas-MacBook-Pro.local>
2023-10-31 12:53:59 +0530

    added question accordion

--- 04-accordion/final/src/Question.js
+++ 04-accordion/final/src/Question.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = ({ title, info }) => {
+const Question = ( title, info ) => {
   const [showInfo, setShowInfo] = useState(false);
   return (
     <article className='question'>
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = () => {
-  return <h2>question component</h2>;
+	const [showInfo, setShowInfo] = useState(false);
+  return (
+  	<article className='question'>
+  		<header>
+  			<h4></h4>
+  			<button className='btn' onClick={() => setShowInfo(!showInfo)}>
+  				{showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+  			</button>
+  		</header>
+  		{ showInfo && <p>a</p>}
+  	</article>
+
+  )
 };
 
 export default Question;

24dc6dfc3629167deb9c91ce8486b29f740cfc1f
Papa <papa@Papas-MacBook-Pro.local>
2023-10-31 17:21:39 +0530

    added left right button jsx

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -3,7 +3,30 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+	const [people, setPeople] = useState(data);
+	const [index, setIndex] = React.useState(0);
+
+	return (
+		<section className="section">
+			<div className="title">
+				<h2>
+					<span>/</span>reviews
+				</h2>
+			</div>
+			<div className="section-center">
+				{
+
+				}
+				<button className="prev" onClick={() => setIndex(index - 1 )}>
+					<FiChevronLeft />
+				</button>
+				<button className="next" onClick={() => setIndex(index + 1)}>
+					<FiChevronRight />
+				</button>
+			</div>
+		</section> 
+	)
 }
+
 
 export default App;

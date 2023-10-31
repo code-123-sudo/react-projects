5643b5eec32ae677f9ec33a957bf7581aaa7cbbe
Papa <papa@Papas-MacBook-Pro.local>
2023-10-31 12:55:28 +0530

    added data in accordion

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
+const Question = (props) => {
 	const [showInfo, setShowInfo] = useState(false);
   return (
   	<article className='question'>
   		<header>
-  			<h4></h4>
+  			<h4>{props.title}</h4>
   			<button className='btn' onClick={() => setShowInfo(!showInfo)}>
   				{showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
   			</button>
   		</header>
-  		{ showInfo && <p>a</p>}
+  		{ showInfo && <p>{props.info}</p>}
   	</article>
 
   )

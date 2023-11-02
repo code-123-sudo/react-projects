bd985f6df950beef2c3fa776dda9e6d4eba73bc4
Papa <papa@Papas-MacBook-Pro.local>
2023-11-02 17:28:07 +0530

    added slider jsx

--- 07-slider/setup/src/Alternative.js
+++ 07-slider/setup/src/Alternative.js
@@ -48,6 +48,28 @@ function App() {
 				</h2>
 			</div>
 			<div className='section-center'>
+			{people.map((person, personIndex) => {
+				const { id, image, name, title, quote} = person
+				let position = 'nextSlide'
+                if (personIndex === index){
+                	position = 'activeSlide'
+                }
+                if (personIndex === index - 1 || (
+                	index === 0 && personIndex === people.length - 1)){
+                	position = 'lastSlide'
+                }
+			return (
+				<article className={position} key={id}>
+					<img src={image} alt={name} className='person-img' />
+				</article>
+			)
+			})}
+				<button className='prev' onClick={prevSlide}>
+					<FiChevronLeft />
+				</button>
+				<button className='next' onClick={nextSlide}>
+					<FiChevronRight />
+				</button>
 			</div>
 		</section>
 	)

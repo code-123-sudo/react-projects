43a28f6c22f8fb18a82a9fc17a0ba34d1f1402c6
Papa <papa@Papas-MacBook-Pro.local>
2023-11-04 17:44:19 +0530

    added input box

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
-
 function App() {
-  return <h2>grocery bud setup</h2>
+	const [name,setName] = useState('')
+ 
+  return (
+  	<section className='section-center'>
+  		<form className='grocery-form' onSubmit={handleSubmit}>
+  		</form>
+  		<h3>grocery bud</h3>
+  		<div className='form-control'>
+  			<input
+  				type='text'
+  				className='grocery'
+  				placeholder='e.g. eggs'
+				value={name}
+				onChange={(e) => setName(e.target.value)}  				
+  		</div>
+  	</section>
+  	)
 }
 
 export default App

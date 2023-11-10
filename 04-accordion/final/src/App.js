9032d66bef4dfb55b66eb99dfb9e8ed0a706d6c4
Papa <papa@Papas-MacBook-Pro.local>
2023-11-10 21:47:43 +0530

    fixes

--- 04-accordion/final/src/App.js
+++ 04-accordion/final/src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  const [questions, setQuestions] = useState(data);
+  const [message, setMessage] = useState('');
   const [userMessages, setUserMessages] = useState([]);
   const handleChange = (event) => {
-    let temp = event.target.value;
-    let temp2 = [...userMessages,temp];
-    console.log(temp2);
+    setMessage(event.target.value)
+  }
+  useEffect(() => { console.log(userMessages)
+    setMessage('');
+   }, [userMessages])
+  const addToArray = () => {
+    setUserMessages(userMessages => [...userMessages,message]);
   }
   return (
     <main>
       <div>
-        <input type='text' onChange={handleChange} />
-        <button type='button'>send</button>
+        {userMessages.map((message) => {
+          return (
+            <div>
+              {message}
+            </div>
+            )
+        })
+
+        }
+        <input type='text' onChange={handleChange} value={message}/>
+        <button type='button' onClick={addToArray}>send</button>
       </div>
     </main>
   );

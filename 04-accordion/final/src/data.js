0487467be447366e26ba94aa8fcba9ddc7a9d62e
Papa <papa@Papas-MacBook-Pro.local>
2023-11-13 14:47:18 +0530

    added dummy questions

--- 04-accordion/final/src/data.js
+++ 04-accordion/final/src/data.js
@@ -1,7 +1,9 @@
 let data = [
-	{ 'question'  : '', 'answer' : ''},
-	{ 'question'  : '', 'answer' : ''},
-	{ 'question'  : '', 'answer' : ''},
-	{ 'question'  : '', 'answer' : ''},
-	{ 'question'  : '', 'answer' : ''},
+	{ question  : 'What is your name', answer : 'Alexa'},
+	{ question  : 'What is your middle name', answer : 'John'},
+	{ question  : 'What is prime number', answer : 'A no. with only two factors.'},
+	{ question  : 'Who is Elon Musk', answer : 'A Enterpreneur.'},
+	{ question  : 'Is Elon Musk married', answer : 'Yes.'},
 ]
+
+export default data;

e2183d2ced4b10815603d61af0353bd7d3987fed
Papa <papa@Papas-MacBook-Pro.local>
2023-11-18 15:15:27 +0530

    fixes

--- 04-accordion/final/src/data.js
+++ 04-accordion/final/src/data.js
@@ -4,6 +4,8 @@ let data = [
 	{ question  : 'What is prime number ?', answer : 'A no. with only two factors.'},
 	{ question  : 'Who is Elon Musk ?', answer : 'A Enterpreneur.'},
 	{ question  : 'Is Elon Musk married ?', answer : 'Yes.'},
+	{ question  : 'israel', answer : 'The Palestinian territories are the two regions of the former British Mandate for Palestine that have been occupied by Israel since the Six-Day War of 1967, namely the West Bank (including East Jerusalem) and the Gaza Strip. The International Court of Justice (ICJ) has referred to the West Bank, including East Jerusalem, as "the Occupied Palestinian Territory", and this term was used as the legal definition by the ICJ in its advisory opinion of July 2004.[7][8] The term occupied Palestinian territory was used by the United Nations and other international organizations between October 1999[9] and December 2012 to refer to areas controlled by the Palestinian National Authority, but from 2012, when Palestine was admitted as one of its non-member observer states, the United Nations started using exclusively the name State of Palestine.[10][11][12][13] The European Union (EU) also uses the term "occupied Palestinian territory".[14][15] The government of Israel and its supporters use the label "disputed territories" instead'},
+
 ]
 
 export default data;
